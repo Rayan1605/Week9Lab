2bcfd7da6526600969450a4d1b7c01ce3e9c033a
G00394595@atu.ie <G00394595@atu.ie>
2024-11-18 13:40:20 +0000

    Check

---
+++ src/main/java/ie/atu/week8/projectexercise/ProductMessageConsumer.java
@@ -0,0 +1,17 @@
+package ie.atu.week8.projectexercise;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Service;
+
+@Service
+@ConditionalOnProperty(name = "consumer.enabled", havingValue = "true")
+public class ProductMessageConsumer {
+
+@RabbitListener(queues = "productQueue")
+    public void processProductMessage(Product product){
+    System.out.println("Received message: " + product.toString());
+}
+
+}
--- src/main/java/ie/atu/week8/projectexercise/ProductService.java
+++ src/main/java/ie/atu/week8/projectexercise/ProductService.java
@@ -1,5 +1,6 @@
 package ie.atu.week8.projectexercise;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +11,13 @@ import java.util.Optional;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final RabbitTemplate rabbitTemplate;
+
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, RabbitTemplate rabbitTemplate) {
         this.productRepository = productRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public List<Product> getAllProducts() {
@@ -25,7 +29,11 @@ public class ProductService {
     }
 
     public Product saveProduct(Product product) {
-        return productRepository.save(product);
+        Product product1 = productRepository.save(product);
+
+        rabbitTemplate.convertAndSend("productQueue", product);
+        System.out.println("Saved Product " + product);
+        return product1;
     }
 
     public void deleteProduct(Long id) {
